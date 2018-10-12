0029f27a66387dbd50646f44f64fd4c6a372e995
User of DK12 <DK12@it.isuct.ru>
2018-10-12 14:47:44 +0400

    2 лабораторная полностью

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace CourseApp
+namespace Matan
 {
     class Program
     {
+        public static double y(double x, double a, double b)
+        {
+            return (Math.Sqrt(Math.Abs(a-b*x)/(Math.Pow(Math.Log10(x),3))));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Aleksandr_Tarasov");
+            double a = 7.2;
+            double b = 4.2;
+            double xn = 1.81;
+            double xk = 5.31;
+            double dx = 0.7;
+
+            double[] x = new double[5] { 2.4, 2.8, 3.9, 4.7, 3.16 };
+
+            Console.WriteLine("Задача A:");
+         
+            for (double xl = xn; xl < xk; xl += dx)
+            {
+                Console.WriteLine($"Для x = {xl}\t y = {y(xl, a, b):f3}");
+            }
+
+            Console.WriteLine("Задача B:");
+            
+            foreach (double i in x)
+            {
+                Console.WriteLine($"Для x = {i}\t y = {y(i, a, b):f3}");
+            }
         }
     }
 }
