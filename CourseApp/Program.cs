194f93ae3616e5ac94c4f67d1d5544386e386118
User of DK12 <DK12@it.isuct.ru>
2018-10-12 14:29:01 +0400

    Second lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -20,21 +20,12 @@ namespace Matan
             double[] x = new double[5] { 0.15, 0.26, 0.37, 0.48, 0.56 };
 
             Console.WriteLine("Задача A:");
-            //do
-            //{
-            //    Console.WriteLine($"Для x = {xn}\t y = {y(xn, a, b)}");
-            //    xn += dx;
-            //} while (xn < xk);
             for (double xl = xn; xl < xk; xl+=dx)
             {
                 Console.WriteLine($"Для x = {xl}\t y = {y(xl, a, b)}");
             }
 
             Console.WriteLine("Задача B:");
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine($"Для x = {x[i]}\t y = {y(x[i], a, b)}");
-            //}
             foreach (double i in x)
             {
                 Console.WriteLine($"Для x = {i}\t y = {y(i, a, b)}");
