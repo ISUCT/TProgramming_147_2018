8148320b7418c0c783b6c75c23f289487fd5f481
User of DK12 <DK12@it.isuct.ru>
2018-10-16 14:18:33 +0400

    second lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CourseApp
+namespace ConsoleApp5
 {
     class Program
     {
+        public static double y(double x, double a, double b)
+        {
+            return (Math.Pow(Math.Log10(x / a), 2) + 1) / (b - Math.Exp(x / a));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Nikolaj_Korolev");
+            double a = 2.0;
+            double b = 0.95;
+            double xn = 1.25;
+            double xk = 2.75;
+            double dx = 0.3;
+
+            double[] x = new double[5] { 2.2,
+                                        3.78,
+                                        4.51,
+                                        6.58,
+                                        1.2 };
+
+            Console.WriteLine("Задача A:");
+            do
+            {
+                Console.WriteLine($"Для x = {xn}\t y = {y(xn, a, b)}");
+                xn += dx;
+            } while (xn < xk);
+
+            Console.WriteLine("Задача B:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Для x = {x[i]}\t y = {y(x[i], a, b)}");
+            }
         }
     }
 }
