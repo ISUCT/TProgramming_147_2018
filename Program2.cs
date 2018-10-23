8a385734ecaac8c76768bcd68b096a92c82ff343
User of DK12 <DK12@it.isuct.ru>
2018-10-23 13:53:46 +0400

    laba 2.v2

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace шишлаб2
+{
+    class Program2
+    {
+
+        static void Main(string[] args)
+        {
+            double num = 1;
+            double x = 1.2, xFinish = 4.2, xStep = 0.6, a = 2, y;
+            while (x < xFinish)
+            {
+                y = (Math.Log(a + x)) * (Math.Log(a + x)) / (a + x) * (a + x);
+                x += xStep;
+                Console.WriteLine($"y{num} = {y}");
+                num++; //счётчик выполнения цикла 
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
