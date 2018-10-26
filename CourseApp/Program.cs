817f2bd15f0a691747d3b8b9ec96c0546d337199
demonbless1337 <43672542+demonbless1337@users.noreply.github.com>
2018-10-26 10:24:30 +0400

    Viktor murashov (#12)
    * I complete my first lab
    
    * Second lab
    
    * Second lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace CourseApp
+namespace Matan
 {
     class Program
     {
+        public static double y(double x, double a, double b)
+        {
+            return (Math.Acos(Math.Pow(x, 2) - Math.Pow(b, 2)) / Math.Asin(Math.Pow(x, 2) - Math.Pow(a, 2)));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Victor_Murashov");
+            double a = 0.05;
+            double b = 0.06;
+            double xn = 0.2;
+            double xk = 0.95;
+            double dx = 0.15;
+
+            double[] x = new double[5] { 0.15, 0.26, 0.37, 0.48, 0.56 };
+
+            Console.WriteLine("Задача A:");
+            for (double xl = xn; xl < xk; xl+=dx)
+            {
+                Console.WriteLine($"Для x = {xl}\t y = {y(xl, a, b)}");
+            }
+
+            Console.WriteLine("Задача B:");
+            foreach (double i in x)
+            {
+                Console.WriteLine($"Для x = {i}\t y = {y(i, a, b)}");
+            }
         }
     }
 }
