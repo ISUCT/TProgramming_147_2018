bc02947ddebc69e9e8e055c1b6f928a9118e699a
User of DK12 <DK12@it.isuct.ru>
2018-10-26 14:00:59 +0400

    laba2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 
-namespace CourseApp
+namespace Matan
 {
-    class Program
+    public class Program
     {
+        public static double y1(double x)
+        {
+            if (Math.Abs(x) >= 1)
+            {
+                return (Math.Pow(1.2, x) - Math.Pow(x, 1.2));
+            }
+            else
+            {
+                return (Math.Acos(x));
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Лебедев Вячеслав Евгеньевич");
+            double xn = 0.2;
+            double xk = 2.2;
+            double dx = 0.4;
+
+            double[] x = new double[5] { 0.1, 0.9, 1.2, 1.5, 2.3 };
+            
+                    Console.WriteLine("Задача A:");
+
+                    for (double xl = xn; xl < xk; xl += dx)
+                    {
+                        Console.WriteLine($"Для x = {xl}\t y = {y1(xl)}");
+                    }
+
+                    Console.WriteLine("Задача B:");
+                    //for (int i = 0; i < 5; i++)
+                    //{
+                    //    Console.WriteLine($"Для x = {x[i]}\t y = {y(x[i], a, b)}");
+                    //}
+                    foreach (double i in x)
+                    {
+                        Console.WriteLine($"Для x = {i}\t y = {y1(i)}");
+                    }
+               
+            
+                
+            
+                
         }
     }
 }
