5551805af4e1ff91da511661eb1c76fd3343b7f2
ebanuktiv <43780037+ebanuktiv@users.noreply.github.com>
2018-10-26 15:08:23 +0400

    laba1 (#24)
    * laba1
    
    * laba2
    
    * laba2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace CourseApp
+namespace Matan
 {
-    class Program
+    public class Program
     {
+        public static double y1(double x)
+        {
+            if (Math.Abs(x) >= 1)
+            {
+                return (Math.Pow(1.2, x) - Math.Pow(x, 1.2));
+            }
+            else
+            {
+                return (Math.Acos(x));
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xn = 0.2;
+            double xk = 2.2;
+            double dx = 0.4;
+
+            double[] x = new double[5] { 0.1, 0.9, 1.2, 1.5, 2.3 };
+            
+                    Console.WriteLine("Задача A:");
+
+                    for (double xl = xn; xl < xk; xl += dx)
+                    {
+                        Console.WriteLine($"Для x = {xl}\t y = {y1(xl)}");
+                    }
+
+                    Console.WriteLine("Задача B:");
+                    
+                    foreach (double i in x)
+                    {
+                        Console.WriteLine($"Для x = {i}\t y = {y1(i)}");
+                    }
+               
+            
+                
+            
+                
         }
     }
 }
