4f554c310bdd3e9152cc5bec06a7572f3b22e991
User of DK12 <DK12@it.isuct.ru>
2018-10-26 14:49:08 +0400

    Выполнил лабораторную работу 3 Тарасов

--- CourseApp.Test/UnitTest1.cs
+++ CourseApp.Test/UnitTest1.cs
@@ -27,32 +27,32 @@ namespace CourseApp.Test
         [Fact]
         public void Test4()
         {
-            var res = Program.y(1, 1, 1);
-            Assert.Equal(Double.NaN, res);
+            var res = Program.y(5, 1, -8);
+            Assert.Equal(10.96, res, 2);
         }
         [Fact]
         public void Test5()
         {
-            var res = Program.y(2, 0, 0);
-            Assert.Equal(0, res);
+            var res = Program.y(2, 3, 0);
+            Assert.Equal(10.49, res, 2);
         }
         [Fact]
         public void Test6()
         {
-            var res = Program.y(0, 0, 9);
+            var res = Program.y(0, 4, -9);
             Assert.Equal(0, res);
         }
         [Fact]
         public void Test7()
         {
-            var res = Program.y(0, 2, 4);
-            Assert.Equal(0, res);
+            var res = Program.y(8, 2, 4);
+            Assert.Equal(6.38, res, 2);
         }
         [Fact]
         public void Test8()
         {
-            var res = Program.y(2, 2, 1);
-            Assert.Equal(0, res);
+            var res = Program.y(2, 7, 1);
+            Assert.Equal(13.54, res, 2);
         }
         [Fact]
         public void Test9()
@@ -63,7 +63,7 @@ namespace CourseApp.Test
         [Fact]
         public void Test10()
         {
-            var res = Program.y(0, 3, 1);
+            var res = Program.y(0, 3, 3);
             Assert.Equal(0, res);
         }
     }
