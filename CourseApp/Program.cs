766b8c47e72725a31dd4e9fa93905c91244d1a67
Your Name <you@example.com>
2018-10-30 14:06:56 +0400

    second lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace CourseApp
+namespace Matan
 {
     class Program
     {
+        public static double y(double x, double a, double b)
+        {
+            return (Math.Asin(Math.Pow(x, a) + Math.Acos(Math.Pow(x, b))));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Anton Naumov!");
+            double a = 2;
+            double b = 3;
+            double xn = 0.11;
+            double xk = 0.36;
+            double dx = 0.05;
+
+            double[] x = new double[5] { 0.08, 0.26, 0.35, 0.41, 0.53 };
+
+            Console.WriteLine("Задача A:");
+            
+            for (double xl = xn; xl < xk; xl += dx)
+            {
+                Console.WriteLine($"Для x = {xl}\t y = {y(xl, a, b)}");
+            }
+
+            Console.WriteLine("Задача B:");
+             
+            foreach (double i in x)
+            {
+                Console.WriteLine($"Для x = {i}\t y = {y(i, a, b)}");
+            }
         }
     }
 }
