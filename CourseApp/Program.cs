b3ed55723bff51e3e25406f1720e9b129fdd0f18
genyn <43672276+genyn@users.noreply.github.com>
2018-11-12 12:02:13 +0400

    Evgenij novozhilov (#30)
    * я выполнил лабораторную работу
    
    * Delete CourseApp.sln
    
    * second ne okonch lab
    
    * second laba
    
    * test

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
-namespace CourseApp
+ namespace Matan
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static double y(double x, double a)
         {
-            Console.WriteLine("Evgenij_Novozhilov");
+            return (Math.Pow(a,(x*x-1)) - (Math.Log(x*x-1)) + (Math.Pow(x*x-1, 1 / 3))); 
+        }
+         static void Main(string[] args)
+        {
+            double a = 1.6;
+            double xn = 1.2;
+            double xk = 3.7;
+            double dx = 0.5;
+             double[] x = new double[5] { 1.28,1.36, 2.47, 3.68, 4.56 };
+             Console.WriteLine("Задача A:");
+      
+            for (double xl = xn; xl < xk; xl += dx)
+            {
+                Console.WriteLine($"Для x = {xl}\t y = {y(xl, a)}");
+            }
+             Console.WriteLine("Задача B:");
+   
+            foreach (double i in x)
+            {
+                Console.WriteLine($"Для x = {i}\t y = {y(i, a)}");
+            }
+            Console.ReadLine();
+
         }
     }
-}
+} 
