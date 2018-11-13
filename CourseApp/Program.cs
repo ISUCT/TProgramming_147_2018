09e0ab53088516dbee59f76ce744d8cea541a0e8
holysneker <43672058+holysneker@users.noreply.github.com>
2018-11-13 14:30:27 +0400

    Update Program.cs

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,39 +1,38 @@
-﻿using System;
+using System;
 
 namespace laba2
 {
     class Program
     {
-                
-                
+
+        static void formula(double x, double a, double b)
+        {
+            double y = ((Math.Pow(a, x) - Math.Pow(b, x)) / (Math.Log10(a / b))) * Math.Pow(Math.Sqrt(a * b), 1 / 3);
+            Console.WriteLine($"При х = {x} функция y = {Math.Round(y, 4)}");
+        }
+
         static void Main()
         {
-
-            Console.WriteLine();
+            
             Console.WriteLine("Первая часть");
             double a = 0.4;
             double b = 0.8;
 
             for (double x = 3.2; x <= 6.2; x = x + 0.6)
             {
-                double y = ((Math.Pow(a, x) - Math.Pow(b, x)) / (Math.Log10(a / b))) * Math.Pow(Math.Sqrt(a * b), 1 / 3);
-                Console.WriteLine($"При х = {x} функция y = {Math.Round(y, 4)}");
+                formula(x, a, b);
             }
 
             Console.WriteLine();
             Console.WriteLine("Вторая часть");
-            
             double[] z = new double[5] { 4.48, 3.56, 2.78, 5.28, 3.21 };
-            for (int i = 0; i <= 4; i++)
+            for (int i = 0; i <= z.Length - 1; i++)
             {
-                double y = ((Math.Pow(a, z[i]) - Math.Pow(b, z[i])) / (Math.Log10(a / b))) * Math.Pow(Math.Sqrt(a * b), 1 / 3);
-                Console.WriteLine($"При х = {z[i]} функция y = {Math.Round(y, 4)}");
+                formula(z[i], a, b);
             }
 
 
             Console.ReadKey();
-
-
         }
 
     }
