a1c93de0b76b9d82d0d24434e4b761c0f1cd0dcc
holysneker <43672058+holysneker@users.noreply.github.com>
2018-11-13 15:40:02 +0400

    first lab (#11)
    * Patch 1 (#8)
    
    * я выполнил лабораторную работу (#3)
    
    * Delete CourseApp.sln
    
    * first lab
    
    * Лаба по фунциям
    
    * Update README.md
    
    * Update Program.cs
    
    * 1 laba (#14)
    
    * 1 laba
    
    * laba2
    
    * laba2
    
    * Delete CourseApp.sln
    
    * Update Program.cs
    
    * Update Program.cs

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 
-namespace CourseApp
+namespace laba2
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void formula(double x, double a, double b)
         {
-            Console.WriteLine("Hello World!");
+            double y = ((Math.Pow(a, x) - Math.Pow(b, x)) / (Math.Log10(a / b))) * Math.Pow(Math.Sqrt(a * b), 1 / 3);
+            Console.WriteLine($"При х = {x} функция y = {Math.Round(y, 4)}");
         }
+
+        static void Main()
+        {
+            
+            Console.WriteLine("Первая часть");
+            double a = 0.4;
+            double b = 0.8;
+
+            for (double x = 3.2; x <= 6.2; x = x + 0.6)
+            {
+                formula(x, a, b);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Вторая часть");
+            double[] z = new double[5] { 4.48, 3.56, 2.78, 5.28, 3.21 };
+            for (int i = 0; i <= z.Length - 1; i++)
+            {
+                formula(z[i], a, b);
+            }
+
+            Console.ReadKey();
+        }
+
     }
 }
