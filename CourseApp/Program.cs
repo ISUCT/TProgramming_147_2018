4871e0af3eb8dcd9c06d4cc3c8a041cc536beff8
User of DK12 <DK12@it.isuct.ru>
2018-12-11 14:01:29 +0400

    laba 4

---
+++ CourseApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    using System;
+
+    public class Mouse
+    {
+        private string name;
+        private string age;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Mouse(string name, string age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Mouse(int name, int age)
+        {
+            this.name = "Noname";
+            this.age = "age";
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name} Возраст: {age}");
+        }
+    }
+}
